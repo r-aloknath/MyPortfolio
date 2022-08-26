97a3c3e85c4dee96e5f6fb8698b98dbd59c0cbff
r-aloknath <aloknath.ar@gmail.com>
2022-08-26 16:43:08 +0530

    form validaion optimized, now scroll to top appear when you scroll down

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+//fetching and validating input fields
 let candName = "";
 let mobileNumber = "";
 let eMail = "";
 let at = "@";
 let com = ".com";
-let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*-/++,.\;:";
 let formStatus = false;
 let emailStatus=false;
 let mobileStatus=false;
+
+//setting error msg to blank
+let mobileError=document.getElementById("mobile-field-error");
+let emailError=document.getElementById("email-field-error");
+let nameError=document.getElementById("name-field-error");
 
 function onSubmitClick() {
   candName = document.getElementById("name").value;
@@ -25,72 +31,91 @@ function onSubmitClick() {
   }
 }
 
+//input validation
 function validateInputs() {
   if (candName == "" || mobileNumber == "" || eMail == "") {
     // validation for blank inputs
+    nameError.innerHTML="fields can not be left blank";
     formStatus = false;
     console.log("fields can not be left blank");
     return formStatus;
   } else {
-    // email validation
-    if(emailValidation()){
-        formStatus=true;
-    }else{
-        formStatus=false;
-    }
+    nameError.innerHTML="";
 
-    // mobile validation
-    if(mobileNumberValidation()){
-        formStatus=true;
+    //checking for email validation 
+    if(emailValidation() && mobileNumberValidation()){
+      formStatus=true;
     }else{
-        formStatus=false;
+      formStatus=false;
     }
-  }
   return formStatus;
+  }
 }
 
+//function to validate email id
 function emailValidation() {
   if (!eMail.includes(at)) {
+    emailError.innerHTML="must contain @";
     console.log("invalid email:must contain @");
     emailStatus = false;
   } else if (eMail.indexOf(at) == 0) {
+    emailError.innerHTML="@ cant be at begining";
     emailStatus = false;
     console.log("@ cant be at begining");
   } else if (!eMail.includes(com)) {
+    emailError.innerHTML="must contain .com";
     emailStatus = false;
     console.log("invalid email:must contain .com");
   } else {
+    emailError.innerHTML="";
     console.log("valid email");
     emailStatus = true;
   }
-
   return emailStatus;
 }
 
 function mobileNumberValidation() {
   if (mobileNumber.length > 10) {
+    mobileError.innerHTML="can't exceed 10 digits";
     mobileStatus = false;
     console.log("invalid mobile number:can't exceed 10 digits");
   } else if (mobileNumber.length < 10) {
+    mobileError.innerHTML="must be of 10 digitsg";
     mobileStatus = false;
     console.log("invalid mobile number:must be of 10 digits");
   } else {
-    for (number of mobileNumber) {
-      for (digit of numbers) {
-        if (number == digit) {
-            mobileStatus = true;
-          continue;
-        } else {
-            mobileStatus = false;
-        }
+    for(char of alphabets){
+      if(mobileNumber.includes(char)){
+        mobileStatus = false;
+        break;;
+      }else{
+        mobileStatus = true;
+        continue;
       }
     }
     if (mobileStatus == true) {
       console.log("valid phone number");
+      mobileError.innerHTML="";
     }else{
+      mobileError.innerHTML="characters not allowed";
         console.log("invalid mobile number:characters not allowed");
     }
   }
-
   return mobileStatus;
+}
+
+//adding scroll effect
+let toTop;
+toTop=document.getElementById("to-top");
+
+window.addEventListener('scroll',()=>{
+  scrollFunction();
+})
+
+function scrollFunction() {
+  if (document.body.scrollTop > 199 || document.documentElement.scrollTop > 199) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
 }
